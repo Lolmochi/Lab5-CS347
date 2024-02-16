60adf83ae35135266971dd4ab56eb7e724245d85
Adison Wongkham <110729522+Lolmochi@users.noreply.github.com>
2024-02-16 10:57:34 +0700

    body-parser

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const app = express()
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
@@ -14,6 +20,7 @@ app.get('/books/:id', (req, res) => {
     res.json(books.find(book => book.id === req.params.id))
   })
   
+
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
